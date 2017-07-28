9a0446e6f0924cbd91be85c8889cbaa95b7d4e88
peterpaints <musonyengigi@gmail.com>
2017-07-28 10:46:39 +0300

    Create argument parser for pagination

---
+++ api/bucketlists/parsers.py
@@ -0,0 +1,6 @@
+from flask_restplus import reqparse
+
+pagination_arguments = reqparse.RequestParser()
+pagination_arguments.add_argument('page', type=int, required=False, default=1, help='Page number')
+pagination_arguments.add_argument('per_page', type=int, required=False, choices=[2, 5, 10, 15, 20, 25, 100],
+                                  default=5, help='Results per page {error_msg}')
