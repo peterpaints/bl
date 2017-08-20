312b662cce7bc24095d86589539df120a3c55102
peterpaints <musonyengigi@gmail.com>
2017-08-20 14:46:17 +0300

    Create a decorator to abstract the check access token validation

---
+++ api/decorators.py
@@ -0,0 +1,18 @@
+from functools import wraps
+
+from flask import abort, request
+
+from models.models import User
+
+
+def check_access_token(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        access_token = request.headers.get('Authorization')
+        if access_token:
+            user_id = User.decode_token(access_token)
+            if isinstance(user_id, str):
+                abort(404, user_id)
+            request.user_id = user_id
+        return func(*args, **kwargs)
+    return wrapper
