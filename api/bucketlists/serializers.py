c07383db07906ad00e0d1ffc435eda5f17422b7c
peterpaints <musonyengigi@gmail.com>
2017-08-20 16:10:57 +0300

    Fix more pep8 errors

--- api/bucketlists/serializers.py
+++ api/bucketlists/serializers.py
@@ -1,9 +1,9 @@
-from flask_restplus import fields
 from api.restplus import api
+from flask_restplus import fields
 
 bucketitems = api.model('Items', {
     'id': fields.Integer,
-    'name': fields.String(required=True, description='The name of a bucketlist item'),
+    'name': fields.String(required=True, description='Name of an item'),
     'bucketlist_id': fields.Integer,
     'date_created': fields.DateTime,
     'date_modified': fields.DateTime,
@@ -11,17 +11,17 @@ bucketitems = api.model('Items', {
 })
 
 create_bucketoritem = api.model('Create', {
-    'name': fields.String(required=True, description='The name of a bucketlist')
+    'name': fields.String(required=True, description='Name of a bucketlist')
 })
 
 edit_item = api.model('Edit', {
-    'name': fields.String(required=True, description='The name of a bucketlist'),
+    'name': fields.String(required=True, description='Name of a bucketlist'),
     'done': fields.Boolean(default=False)
 })
 
 buckets = api.model('Bucketlists', {
     'id': fields.Integer,
-    'name': fields.String(required=True, description='The name of a bucketlist'),
+    'name': fields.String(required=True, description='Name of a bucketlist'),
     'items': fields.List(fields.Nested(bucketitems)),
     'date_created': fields.DateTime,
     'date_modified': fields.DateTime,
