126a72011c4300e443b17b531e4109eac7aa2466
peterpaints <musonyengigi@gmail.com>
2017-08-20 14:59:05 +0300

    Use check_access_token decorator to abstract token validation

--- api/bucketlists/endpoints/bucketlists.py
+++ api/bucketlists/endpoints/bucketlists.py
@@ -1,10 +1,11 @@
 from flask import request
-from flask_restplus import Resource, abort
-from api.bucketlists.serializers import create_bucketoritem, buckets
-from api.bucketlists.parsers import pagination_arguments
-from api.restplus import api
-from models.models import Bucketlist, User
 
+from api.bucketlists.parsers import pagination_arguments
+from api.bucketlists.serializers import buckets, create_bucketoritem
+from api.decorators import check_access_token
+from api.restplus import api
+from flask_restplus import Resource, abort
+from models.models import Bucketlist
 
 ns = api.namespace('bucketlists', description='Bucketlist manipulation')
 
@@ -15,132 +16,83 @@ ns = api.namespace('bucketlists', description='Bucketlist manipulation')
 @api.header('Authorization', 'JSON Web Token', required=True)
 class BucketListEndPoint(Resource):
 
+    @check_access_token
     @api.expect(pagination_arguments, validate=True)
     @api.marshal_list_with(buckets)
     def get(self):
         """Return list of bucketlists."""
-        # Get the access token from the header
-        access_token = request.headers.get('Authorization')
+        args = pagination_arguments.parse_args(request)
+        page = args.get('page', 1)
+        per_page = args.get('per_page', 5)
+        find = args.get('q')
 
-        if access_token:
-            # Attempt to decode the token and get the User ID
-            user_id = User.decode_token(access_token)
-
-            if not isinstance(user_id, str):
-                args = pagination_arguments.parse_args(request)
-                page = args.get('page', 1)
-                per_page = args.get('per_page', 5)
-                find = args.get('q')
-                # Go ahead and handle the request, the user is authenticated
-                # GET all the bucketlists created by this user
-                try:
-                    bucketlists_query = Bucketlist.query.filter_by(created_by=user_id)
-                    if find:
-                        bucketlists = bucketlists_query.filter(Bucketlist.name.ilike('%' + find + '%'))
-                        return bucketlists.paginate(page, per_page, error_out=False).items, 200
-                    else:
-                        bucketlists = bucketlists_query.paginate(page, per_page, error_out=False)
-                        return bucketlists.items, 200
-                except Exception as e:
-                    abort(404, str(e))
+        # GET all the bucketlists created by this user
+        try:
+            bucketlists_query = Bucketlist.query.filter_by(created_by=request.user_id)
+            if find:
+                bucketlists = bucketlists_query.filter(Bucketlist.name.ilike('%' + find + '%'))
+                return bucketlists.paginate(page, per_page, error_out=False).items, 200
             else:
-                # user is not legit, so the payload is an error message
-                abort(401, user_id)
+                bucketlists = bucketlists_query.paginate(page, per_page, error_out=False)
+                return bucketlists.items, 200
+        except Exception as e:
+            abort(404, str(e))
 
+    @check_access_token
     @api.expect(create_bucketoritem)
     @api.marshal_with(buckets)
     def post(self):
         """Create a bucketlist."""
-        # Get the access token from the header
-        access_token = request.headers.get('Authorization')
 
-        if access_token:
-            # Attempt to decode the token and get the User ID
-            user_id = User.decode_token(access_token)
+        name = request.json.get('name')
+        if name:
+            bucketlist = Bucketlist(name=name, created_by=request.user_id)
+            bucketlist.save()
 
-            if not isinstance(user_id, str):
-                # Go ahead and handle the request, the user is authenticated
-
-                name = request.json.get('name')
-                if name:
-                    bucketlist = Bucketlist(name=name, created_by=user_id)
-                    bucketlist.save()
-
-                    return bucketlist, 201
+            return bucketlist, 201
 
 
 @ns.route('/<int:id>')
 @api.header('Authorization', 'JSON Web Token', required=True)
 class BucketlistManipulation(Resource):
 
+    @check_access_token
     @api.marshal_with(buckets)
     def get(self, id):
         """Return the bucketlist with the provided id."""
-        # get the access token from the authorization header
-        access_token = request.headers.get('Authorization')
 
-        if access_token:
-            # Get the user id related to this access token
-            user_id = User.decode_token(access_token)
+        # Get the bucketlist with the id specified from the URL (<int:id>)
+        bucketlist = Bucketlist.query.filter_by(id=id, created_by=request.user_id).first()
+        if bucketlist:
+            return bucketlist, 200
+            # There is no bucketlist with this ID for this User, so
+            # Raise an HTTPException with a 404 not found status code
+        else:
+            abort(404, 'There is no bucketlist with id ' + str(id))
 
-            if not isinstance(user_id, str):
-                # If the id is not a string(error), we have a user id
-                # Get the bucketlist with the id specified from the URL (<int:id>)
-                bucketlist = Bucketlist.query.filter_by(id=id, created_by=user_id).first()
-                if bucketlist:
-                    return bucketlist, 200
-                    # There is no bucketlist with this ID for this User, so
-                    # Raise an HTTPException with a 404 not found status code
-                else:
-                    abort(404, 'There is no bucketlist with id ' + str(id))
-            else:
-                abort(401, user_id)
-
+    @check_access_token
     @api.expect(create_bucketoritem)
     @api.marshal_with(buckets)
     def put(self, id):
         """Edit the name of a bucketlist."""
-        access_token = request.headers.get('Authorization')
 
-        if access_token:
-            # Get the user id related to this access token
-            user_id = User.decode_token(access_token)
+        name = request.json.get('name')
+        bucketlist = Bucketlist.query.filter_by(id=id, created_by=request.user_id).first()
+        if not bucketlist:
+            abort(404, 'This user has no bucketlist with id ' + str(id))
+        if name:
+            bucketlist.name = name
+            bucketlist.save()
 
-            if not isinstance(user_id, str):
+            return bucketlist, 200
 
-                name = request.json.get('name')
-                bucketlist = Bucketlist.query.filter_by(id=id, created_by=user_id).first()
-                if not bucketlist:
-                    abort(404, 'This user has no bucketlist with id ' + str(id))
-                if name:
-                    if bucketlist:
-                        bucketlist.name = name
-                        bucketlist.save()
-
-                        return bucketlist, 200
-                        # There is no bucketlist with this ID for this User, so
-                        # Raise an HTTPException with a 404 not found status code
-                    else:
-                        abort(404, 'There is no bucketlist with id ' + str(id))
-            else:
-                abort(401, user_id)
-
+    @check_access_token
     def delete(self, id):
         """Delete a particular bucketlist."""
-        access_token = request.headers.get('Authorization')
 
-        if access_token:
-            # Get the user id related to this access token
-            user_id = User.decode_token(access_token)
-
-            if not isinstance(user_id, str):
-                bucketlist = Bucketlist.query.filter_by(id=id, created_by=user_id).first()
-                if not bucketlist:
-                    abort(404, 'This user has no bucketlist with id ' + str(id))
-                if bucketlist:
-                    bucketlist.delete()
-                    return 'Bucketlist id ' + str(id) + ' successfully deleted', 200
-                else:
-                    abort(404, 'There is no bucketlist with id ' + str(id))
+        bucketlist = Bucketlist.query.filter_by(id=id, created_by=request.user_id).first()
+        if not bucketlist:
+            abort(404, 'This user has no bucketlist with id ' + str(id))
         else:
-            abort(401, user_id)
+            bucketlist.delete()
+            return 'Bucketlist id ' + str(id) + ' successfully deleted', 200
