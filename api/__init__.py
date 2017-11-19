2fbbb733b44cdd930fd3dbfd2b84da70c7d0ab71
peterpaints <musonyengigi@gmail.com>
2017-11-20 01:02:21 +0300

    [chore] Enable CORS

--- api/__init__.py
+++ api/__init__.py
@@ -5,10 +5,12 @@ from api.auth.views import ns as auth_namespace
 from api.bucketlists.endpoints.bucketlists import ns as bucketlists_namespace
 from api.bucketlists.endpoints.items import ns as items_namespace
 from models.models import db
+from flask_cors import CORS
 
 
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
+    CORS(app)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
