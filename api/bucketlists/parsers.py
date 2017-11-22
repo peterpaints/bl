26fa26dd5440031a213cfd806b54285db72d8ac4
peterpaints <musonyengigi@gmail.com>
2017-11-22 10:54:35 +0300

    [Add '12' to page choices]

--- api/bucketlists/parsers.py
+++ api/bucketlists/parsers.py
@@ -9,7 +9,7 @@ pagination_arguments.add_argument('page',
 pagination_arguments.add_argument('per_page',
                                   type=int,
                                   required=False,
-                                  choices=[2, 5, 10, 15, 20, 25, 100],
+                                  choices=[2, 5, 10, 12, 15, 20, 25, 100],
                                   default=5,
                                   help='Results per page {error_msg}')
 pagination_arguments.add_argument('q', required=False, help='Search term')
